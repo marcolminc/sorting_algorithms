6021bbb53120b9503634dd2c5539e705da644798
marcolminc <thommarcolminc@gmail.com>
2026-01-17 01:16:16 +0300

    use a single buffer for left and right subarrays

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,5 +1,5 @@
 #include "sort.h"
-
+#include <stdlib.h>
 
 /**
  * merge - merges two sorted arrays into one larger array
@@ -8,35 +8,35 @@
  * @l_len: size of left
  * @right: the right sub-array after division
  * @r_len: size of right
- *
- * Return: nothing
  */
 void merge(int *array, int *left, size_t l_len, int *right, size_t r_len)
 {
-	size_t i, j, k;
+    size_t i, j, k;
 
-	i = j = k = 0;
+    i = j = k = 0;
 
-	printf("Merging...\n");
-	printf("[left]: ");
-	print_array(left, l_len);
-	printf("[right]: ");
-	print_array(right, r_len);
+    printf("Merging...\n");
+    printf("[left]: ");
+    print_array(left, l_len);
+    printf("\n[right]: ");
+    print_array(right, r_len);
+    printf("\n");
 
-	while (i < l_len && j < r_len)
-	{
-		if (left[i] <= right[j])
-			array[k++] = left[i++];
-		else
-			array[k++] = right[j++];
-	}
-	while (i < l_len)
-		array[k++] = left[i++];
-	while (j < r_len)
-		array[k++] = right[j++];
+    while (i < l_len && j < r_len)
+    {
+        if (left[i] <= right[j])
+            array[k++] = left[i++];
+        else
+            array[k++] = right[j++];
+    }
+    while (i < l_len)
+        array[k++] = left[i++];
+    while (j < r_len)
+        array[k++] = right[j++];
 
-	printf("[Done]: ");
-	print_array(array, l_len + r_len);
+    printf("[Done]: ");
+    print_array(array, l_len + r_len);
+    printf("\n");
 }
 
 /**
@@ -44,27 +44,29 @@ void merge(int *array, int *left, size_t l_len, int *right, size_t r_len)
  * merge sort algorithm (top-down)
  * @array: the array given to sort
  * @size: length (size) of array
- *
- * Return: nothing
- * - when an array is divided, the size of left sub-array should always be
- * <= the size of right sub-array
- * - left sub-array is sorted before the right sub-array
- * - allowed to use malloc and free only once (only one call)
  */
 void merge_sort(int *array, size_t size)
 {
-	size_t mid, i;
-	int left[size / 2], right[size - (size / 2)];
-	if (size <= 1)
-		return;
-	mid = size / 2;
-	for (i = 0; i < mid; i++)
-		left[i] = array[i];
-	for (i = mid; i < size; i++)
-		right[i - mid] = array[i];
+    size_t mid, i;
+    int *buffer;
 
-	merge_sort(left, mid);
-	merge_sort(right, size - mid);
+    if (size <= 1)
+        return;
 
-	merge(array, left, mid, right, size - mid);
+    mid = size / 2;
+    buffer = malloc(size * sizeof(int));
+    if (!buffer)
+        return; /* Handle allocation failure */
+
+    for (i = 0; i < mid; i++)
+        buffer[i] = array[i];
+    for (i = mid; i < size; i++)
+        buffer[i] = array[i];
+
+    merge_sort(buffer, mid);
+    merge_sort(buffer + mid, size - mid);
+
+    merge(array, buffer, mid, buffer + mid, size - mid);
+    free(buffer);
 }
+
